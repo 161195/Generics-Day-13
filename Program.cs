2a576e369a4b2b09845a46e1aaac7f1615ab172b
“mayuri” <mayurisalunkhe01@gmail.com>
2021-10-23 14:38:33 +0530

    adding welcome msg to generics

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Generics_problem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to Generic Problem!");
+        }
+    }
+}
